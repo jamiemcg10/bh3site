263a97b102c4f328d1940b7a8326fe66c3a8c935
Brendan Caffrey <brendan@devspade.com>
2014-04-30 20:13:00 -0400

    remove site_sections

--- app/models/site_section.rb
+++
@@ -1,9 +0,0 @@
-class SiteSection < ActiveRecord::Base
-  validates :tag, presence: true
-  validates :value, presence: true
-  validates :data_type, presence: true
-  
-  validates :data_type, inclusion: { in: %w(text html markdown),
-    message: "%{value} is not a valid data_type" }
-  
-end
--- app/views/site_sections/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @site_section, :tag, :data_type, :value, :created_at, :updated_at
