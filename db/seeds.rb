65c0fa9fab6ef03f2d0edb7f7b9d3937bf9508cc
Matt Clement <darkmoves@gmail.com>
2014-12-18 21:00:20 -0500

    Update marathon 2015 special event

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # Create the BH3 Marathon 2015 event
-SpecialEvent.create(name: "BH3 Marathon 2015", date: Date.parse("2015-04-18"), url_code: "marathon")
+SpecialEvent.create(name: "BH3 Marathon 2015", date: Date.parse("2015-04-18"), url_code: "marathon2015")
