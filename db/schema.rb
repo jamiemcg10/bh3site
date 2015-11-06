02fbb582e34fb54065172a0e53f92aafc0ad62a9
Brendan <brendan@Devspade.com>
2015-11-05 22:37:38 -0500

    humor address was listed as runs address

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,9 @@ ActiveRecord::Schema.define(version: 20141209130717) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -73,8 +76,6 @@ ActiveRecord::Schema.define(version: 20141209130717) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
