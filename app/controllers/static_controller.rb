334cd38e0bf51541e6bc85623ea264552d936110
Brendan <brendan@Devspade.com>
2015-11-28 15:51:23 -0500

    change key to browser key, use Chrome user agent

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@
 require 'open-uri'
 
 class StaticController < ApplicationController
-  CAL_URL = "https://www.googleapis.com/calendar/v3/calendars/bostonhash@gmail.com/events?key=AIzaSyCMZCb6nbsoAyOwuBDD2pRrvMf1rjYSANc&futureevents=true&orderby=starttime&sortorder=a&singleevents=true&showDelete=false&singleEvents=true&orderBy=startTime&timeMin="
+  CAL_URL = "https://www.googleapis.com/calendar/v3/calendars/bostonhash@gmail.com/events?key=AIzaSyC93cQGDRfpguqdsNMIEEYRKXeIZzcgr28&futureevents=true&orderby=starttime&sortorder=a&singleevents=true&showDelete=false&singleEvents=true&orderBy=startTime&timeMin="
 
   def welcome
     if !params[:id] then
@@ -26,7 +26,7 @@ class StaticController < ApplicationController
     def get_next_hash(id)
       begin
           url = CAL_URL + DateTime.now.beginning_of_day.iso8601
-          cal_results = open(url,'User-Agent' => 'bh3')
+          cal_results = open(url,'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36')
           ev = ActiveSupport::JSON.decode(cal_results)["items"][id]
           Event.new ev
       rescue 
