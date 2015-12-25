993998dee0810772ba13bcb918985b252795b3c6
Matt Clement <darkmoves@gmail.com>
2015-12-25 12:56:06 -0500

    Remove marathon 2015 from seeds and add marathon 2016

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # Create the BH3 Marathon 2015 event
-SpecialEvent.create(name: "BH3 Marathon 2015", date: Date.parse("2015-04-18"), url_code: "marathon2015")
+SpecialEvent.create(name: "BH3 Marathon 2016", date: Date.parse("2016-04-16"), url_code: "marathon2016")
