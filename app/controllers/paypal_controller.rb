719bf1c57863cb421c51314fbc2039132270cbd7
Brendan <brendan@Devspade.com>
2015-12-27 20:49:38 -0500

    first try at catching a webhook

---
+++ app/controllers/paypal_controller.rb
@@ -0,0 +1,20 @@
+require 'paypal-sdk-rest'
+include PayPal::SDK::REST
+include PayPal::SDK::Core::Logging
+
+class PayPalController < ApplicationController
+	
+	# this guy will catch the paypal webhooks
+	def catch
+		 r = request.body.read
+		 log(r)
+		 render :text => r
+	end
+
+
+	private
+
+		# little bit of validation to make sure it's actually paypal posting to us and not some jabroni 
+		def validate
+		end
+end
