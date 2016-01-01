0bab1143ed9ef2ec8985b8b510c84cd1b5febfb4
Brendan <brendan@Devspade.com>
2016-01-01 11:30:34 -0500

    first crack at ipn validation

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -1,6 +1,10 @@
+require "uri"
+require "net/http"
+
 class PayController < ApplicationController
 	protect_from_forgery except: [:catch]
 
+	IPN_VALIDATE_URL = "https://www.sandbox.paypal.com/cgi-bin/webscr"
 	PAY_CONFIRM_URL = "http://google.com"
 	PAY_EMAIL = "brendan.caffrey@gmail.com"
 
@@ -8,6 +12,8 @@ class PayController < ApplicationController
 	def catch
 		  params.permit! # Permit all Paypal input params
 		  pp_status = params[:payment_status]
+		  raw = request.raw_post
+		  validate raw
 		  if pp_status == "Completed"
 		  	  payer =  params[:payer_email]
 		  	  txn_id = params[:txn_id]
@@ -44,6 +50,22 @@ class PayController < ApplicationController
 	    }
 	    "https://sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
 	  end
+
+	  # take raw ipn and validate it
+	  def validate_ipn(raw)
+	  	post_data = "cmd=_notify-validate&" + raw
+	  	url = URI.parse(IPN_VALIDATE_URL)
+	  	http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+
+		request = Net::HTTP::Post.new(uri.path)
+		request.body = post_data
+
+		response = http.request(request)
+		puts response
+	  end
+	  	
+	  
 
 
 end
