299e1b3c43c5635cd2aa6078f4fe1c213a0b5ab4
Matt Clement <darkmoves@gmail.com>
2016-01-08 00:34:16 -0500

    Dynamically set price according to date and additional swag.

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -1,12 +1,23 @@
 class EventRegistration < ActiveRecord::Base
   FOOD_OPTIONS = ["Carnivore", "Vegetarian"]
   SHIRT_SIZES = ["S", "M", "L", "XL"]
+  HAB_PRICE = 15
 
   validates :contact_email, :hash_name, :nerd_name, :kennel, :payment_email, presence: true
 
   belongs_to :special_event
 
   after_initialize :set_defaults, if: :new_record?
+
+  def self.rego_price
+    if Date.current < Date.parse("2016-02-10")
+      69
+    elsif Date.current < Date.parse("2016-04-01")
+      79
+    else
+      89
+    end
+  end
 
   private
 
