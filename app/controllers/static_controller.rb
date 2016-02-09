ccf70170d29535a8a8519ac4a5aa2c8013268de2
Matt Clement <darkmoves@gmail.com>
2016-02-09 18:13:22 -0500

    Update JSON parsing of calendar url per rails upgrade

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,4 @@
-
-require 'open-uri'
+require "open-uri"
 
 class StaticController < ApplicationController
   CAL_URL = "https://www.googleapis.com/calendar/v3/calendars/bostonhash@gmail.com/events?key=AIzaSyCMZCb6nbsoAyOwuBDD2pRrvMf1rjYSANc&futureevents=true&orderby=starttime&sortorder=a&singleevents=true&showDelete=false&singleEvents=true&orderBy=startTime&timeMin="
@@ -8,8 +7,8 @@ class StaticController < ApplicationController
     if !params[:id] then
       params[:id] = 0
     end
-
-   @next_hash = get_next_hash(params[:id].to_f)
+    
+    @next_hash = get_next_hash(params[:id].to_f)
   end
   
   def more
@@ -22,14 +21,13 @@ class StaticController < ApplicationController
   end
   
   private 
-    def get_next_hash(id)
-      begin  
-          url = CAL_URL + Time.zone.now.beginning_of_day.iso8601
-          cal_results = open(url)
-          ev = ActiveSupport::JSON.decode(cal_results)["items"][id]
-          Event.new ev
-      rescue Exception => e
-          puts e
-      end
-    end
+
+  def get_next_hash(id)
+      url = CAL_URL + Time.zone.now.beginning_of_day.iso8601
+      cal_results = open(url)
+      ev = JSON.parse(File.open(cal_results).read)["items"][0]
+      Event.new ev
+  rescue Exception => e
+      puts e
+  end
 end
