c961d58a34dbf54e2a1b2e4f886172f7b664fd7a
Matt Clement <darkmoves@gmail.com>
2016-02-18 12:23:10 -0500

    Update seed file with new attributes for BH3 Marathon 2016 event

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # Create the BH3 Marathon 2016 event
-SpecialEvent.create(name: "Boston H3 Marathon 2016: Dungeons and Drag Queens", date: Date.parse("2016-04-16"), url_code: "marathon_2016")
+SpecialEvent.create(name: "Boston H3 Marathon 2016: Dungeons and Drag Queens", date: Date.parse("2016-04-16"), url_code: "marathon_2016", tiered_rego_prices: [69, 79], tiered_rego_dates: [Date.parse("2016-02-09"), Date.parse("2016-03-31")], full_rego_price: 89)
