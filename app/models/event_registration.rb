467bba16b1dcc79b169c45cb9b00662a25ae2f8a
Matt Clement <darkmoves@gmail.com>
2016-02-18 15:05:03 -0500

    Move food options from event_registration model to special_event model

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -1,6 +1,4 @@
 class EventRegistration < ActiveRecord::Base
-  FOOD_OPTIONS = ["Carnivore", "Vegetarian"]
-  SHIRT_SIZES = ["S", "M", "L", "XL"]
   HAB_PRICE = 15
 
   validates :contact_email, :hash_name, :nerd_name, :kennel, :payment_email, presence: true
