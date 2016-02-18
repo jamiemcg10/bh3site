58d7fb175ccc388594b572ca8109c9f49741bfd1
Matt Clement <darkmoves@gmail.com>
2016-02-18 12:22:36 -0500

    Remove event price logic from event registration model and spec

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -2,27 +2,12 @@ class EventRegistration < ActiveRecord::Base
   FOOD_OPTIONS = ["Carnivore", "Vegetarian"]
   SHIRT_SIZES = ["S", "M", "L", "XL"]
   HAB_PRICE = 15
-  FIRST_TIER_DATE = Date.parse("2016-02-10")
-  SECOND_TIER_DATE = Date.parse("2016-04-01")
-  FIRST_TIER_PRICE = 69
-  SECOND_TIER_PRICE = 79
-  DEFAULT_PRICE = 89
 
   validates :contact_email, :hash_name, :nerd_name, :kennel, :payment_email, presence: true
 
   belongs_to :special_event
 
   after_initialize :set_defaults, if: :new_record?
-
-  def self.rego_price
-    if Date.current < FIRST_TIER_DATE
-      FIRST_TIER_PRICE
-    elsif Date.current < SECOND_TIER_DATE
-      SECOND_TIER_PRICE
-    else
-      DEFAULT_PRICE
-    end
-  end
 
   private
 
--- spec/models/event_registration_spec.rb
+++ spec/models/event_registration_spec.rb
@@ -17,32 +17,4 @@ describe EventRegistration do
     its(:registration_date) { should eq Date.current }
     its(:paid)              { should be false }
   end
-
-  describe ".rego_price" do
-    after { travel_back }
-
-    context "date is before first tier rego date" do
-      before { travel_to EventRegistration::FIRST_TIER_DATE - 1.day }
-
-      it "sets price at the first tier" do
-        expect(EventRegistration.rego_price).to eq EventRegistration::FIRST_TIER_PRICE
-      end
-    end
-
-    context "date is between first and second tier rego dates" do
-      before { travel_to EventRegistration::SECOND_TIER_DATE - 1.day }
-
-      it "sets the price at the second tier" do
-        expect(EventRegistration.rego_price).to eq EventRegistration::SECOND_TIER_PRICE
-      end
-    end
-
-    context "date is after second tier rego date" do
-      before { travel_to EventRegistration::SECOND_TIER_DATE + 1.day }
-
-      it "sets the price at the default tier" do
-        expect(EventRegistration.rego_price).to eq EventRegistration::DEFAULT_PRICE
-      end
-    end
-  end
 end
