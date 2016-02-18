7f8b86f1a66a3cf9621a81fe49fefaaadcacded2
Matt Clement <darkmoves@gmail.com>
2016-02-18 11:37:00 -0500

    Add JSON option to serialization of event attributes

--- app/models/special_event.rb
+++ app/models/special_event.rb
@@ -1,8 +1,8 @@
 class SpecialEvent < ActiveRecord::Base
   validates :name, :date, :url_code, presence: true
 
-  serialize :tiered_rego_prices, Array
-  serialize :tiered_rego_dates, Array
+  serialize :tiered_rego_prices, JSON
+  serialize :tiered_rego_dates, JSON
 
   has_many :event_registrations
 
