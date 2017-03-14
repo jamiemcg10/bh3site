e37d8d3380a4d7c631eb4e3b90b8b2236db29fd3
Matt Clement <darkmoves@gmail.com>
2017-03-14 19:20:30 -0400

    Add Delete Rego button to mismanagement show view for event registrations

--- app/admin/event_registration.rb
+++ app/admin/event_registration.rb
@@ -1,11 +1,23 @@
 ActiveAdmin.register EventRegistration do
+  ACTIVE = 0
+
   permit_params :contact_email, :hash_name, :nerd_name, :kennel, :payment_email, :food_preference, :gluten_allergy, :need_crash_space, :extra_hab, :extra_hab_size, :registration_date, :paid
 
   actions :all, except: :destroy
 
+  action_item only: [:show, :edit] do
+    link_to "Delete Rego", delete_registration_path(id: params[:id]), method: :post
+  end
+
   SpecialEvent.all.order(date: :desc).each do |event|
     scope event.name.to_sym do |registrations|
       registrations.for_event(event)
+    end
+  end
+
+  controller do
+    def scoped_collection
+      super.where(status: ACTIVE)
     end
   end
 
@@ -40,6 +52,24 @@ ActiveAdmin.register EventRegistration do
     column :registration_date
     column :rego_price
     column :paid
+    column :status
     actions
+  end
+
+  form do |f|
+    f.inputs "Change the Fucking Event Regos Here, Hash Mothafuckas!" do
+      f.input :special_event
+      f.input :contact_email
+      f.input :hash_name
+      f.input :nerd_name
+      f.input :kennel
+      f.input :payment_email
+      f.input :food_preference
+      f.input :extra_hab
+      f.input :extra_hab_size
+      f.input :registration_date
+      f.input :rego_price
+    end
+    f.actions
   end
 end
