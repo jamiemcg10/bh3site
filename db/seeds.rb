0d857fc84d0861a4f8a839e6c0d1541fd73ace4e
steve stetak <steve.stetak@gmail.com>
2019-01-22 19:03:15 -0500

    fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,7 @@ end
 # Create the BH3 Marathon 2019 event unless it exists
 if !SpecialEvent.find_by(name: "Boston H3 Marathon 2019 - Game of Boners: A Song of Fireball and Smirnoff Ice")
   SpecialEvent.create(
-    name: "Boston H3 Marathon 2019 - Game of Boners: A Song of Fireball and Smirnoff Ice")",
+    name: "Boston H3 Marathon 2019 - Game of Boners: A Song of Fireball and Smirnoff Ice",
     date: Date.parse("2018-04-13"),
     url_code: "marathon_2019",
     tiered_rego_prices: [69, 79],
