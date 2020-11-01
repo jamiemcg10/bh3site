b2a623f6b1838e3fdd7ca37c7b5c5611fdad724e
Brendan <brendan@Devspade.com>
2020-11-01 07:26:15 -0500

    pull keys out to variables to start, separate server/client keys

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,11 @@
-require "open-uri"
+require 'open-uri'
 
 class StaticController < ApplicationController
-  CAL_URL = "https://www.googleapis.com/calendar/v3/calendars/bostonhash@gmail.com/events?key=AIzaSyCBpRgYGNS0WkDcRC9VsMW59tKL5UzgE0o&futureevents=true&orderby=starttime&sortorder=a&singleevents=true&showDelete=false&singleEvents=true&orderBy=startTime&timeMin="
+  CAL_URL = 'https://www.googleapis.com/calendar/v3/calendars/bostonhash@gmail.com/events?futureevents=true&orderby=starttime&sortorder=a&singleevents=true&showDelete=false&singleEvents=true&orderBy=startTime&timeMin='
+
+  CLIENT_API_KEY = 'AIzaSyCBpRgYGNS0WkDcRC9VsMW59tKL5UzgE0o'
+  SERVER_API_KEY = 'AIzaSyAXn8LFippkNm2MtluPBppBbrVLxczBro4'
+
 
   def welcome
     @next_hash = get_next_hash(id)
@@ -20,7 +24,7 @@ class StaticController < ApplicationController
   end
   
   def calendar
-    @api_key = 'AIzaSyCBpRgYGNS0WkDcRC9VsMW59tKL5UzgE0o' #'ENV["GOOGLE_API_KEY"]'
+    @api_key =  CLIENT_API_KEY #'ENV['GOOGLE_API_KEY']'
   end
   
   private 
@@ -31,20 +35,29 @@ class StaticController < ApplicationController
 
   def get_next_hash(id)
       
-      url = CAL_URL + Time.zone.now.beginning_of_day.iso8601
+      url = calendar_url
+      puts "cal url == " + url
       cal_results = open(url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
-      ev = JSON.parse(cal_results)["items"][id]
+      ev = JSON.parse(cal_results)['items'][id]
       next_ev = Event.new ev
 
       # From best I can tell this is a google cal api bug, we're occasionally getting an item from the previous day when we should not.
       # Typically this isn't a big deal and sorts itself in a day or so.  But when we have lots of events in a short span - ie marathon week - this is problematic
       # In fairness to them I think this feed we use is some kind of forgotten back door
       if id == 0 and next_ev.when < Time.zone.now.beginning_of_day then
-        ev = JSON.parse(cal_results)["items"][id + 1]
+        ev = JSON.parse(cal_results)['items'][id + 1]
         next_ev = Event.new ev
       end
       return next_ev
   rescue Exception => e
       puts e
+  end
+
+  private
+  
+  def calendar_url
+    @cal_url = CAL_URL + Time.zone.now.beginning_of_day.iso8601
+    @cal_url << '&key=' + SERVER_API_KEY
+    return @cal_url
   end
 end
